6f85df9d516683df14a111dbcedcbef65487947e
v8p1197 <v.petrone6@studenti.unisa.it>
2020-11-13 20:50:21 +0100

    head node implemented

--- pepper_pkg/src/head_node/main.py
+++ pepper_pkg/src/head_node/main.py
@@ -1,8 +1,45 @@
 #!/usr/bin/python
+import numpy as np
+import rospy
+from naoqi_driver.naoqi_node import NaoqiNode
+from pepper_msgs.srv import LookAt, LookAtResponse
+
+
+class HeadNode(NaoqiNode):
+
+  CAMERA_FOV = np.deg2rad(55.2)
+
+  __slots__ = 'motionProxy'
+
+  def __init__(self):
+    NaoqiNode.__init__(self, 'master_node')
+    self.connectNaoQi()
+
+  def connectNaoQi(self):
+    rospy.loginfo("HeadNode connecting to NaoQi at %s:%d", self.pip, self.pport)
+    self.motionProxy = self.get_proxy("ALMotion")
+    if self.motionProxy is None:
+      exit(1)
+  
+  def start(self):
+    rospy.Service(rospy.get_param('look_at_service'), LookAt, self.handle_look_at)
+
+  def handle_look_at(self, req):
+    self.motionProxy.setStiffnesses("Head", 1.0)
+    names  = ["HeadPitch", "HeadYaw"]
+    angles  = [0, self.CAMERA_FOV * req.direction]
+    fractionMaxSpeed  = 0.5
+    self.motionProxy.setAngles(names, angles, fractionMaxSpeed)
+    while True:
+      currentAngles = self.motionProxy.getAngles(names, True)
+      if np.all(np.array(angles) - np.array(currentAngles) < np.array([1e-3, 1e-3])):
+        break
+    return LookAtResponse(True)
+
 
 def main():
-    pass
+  HeadNode().start()
 
 
 if __name__ == "__main__":
-    main()
+  main()
