990d12a4b30d0c52b4f4971d679381ab0770d90f
v8p1197 <v.petrone6@studenti.unisa.it>
2020-11-24 22:54:58 +0100

    added documentation for master node

--- pepper_pkg/src/master_node/main.py
+++ pepper_pkg/src/master_node/main.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python
+"""
+This node coordinates the executions of the Head Node and the Camera Node,
+as the images need to be taken when the head is in proper positions.
+"""
+
 import rospy
 from naoqi_driver.naoqi_node import NaoqiNode
 from pepper_msgs.srv import LookAt
 from pepper_msgs.srv import TakePicture
 from std_msgs.msg import Int8
 
-
+"""Class used as an abstraction of the Node"""
 class MasterNode(NaoqiNode):
 
   __slots__ = 'motionProxy'
 
   def __init__(self):
+    """
+    Constructor. Creates the node and connects it to the NaoQi interface.
+    """
     NaoqiNode.__init__(self, 'master_node')
     self.connectNaoQi()
 
   def connectNaoQi(self):
+    """
+    Connects the node to the NaoQi interface. The parameters pip and pport are stored in the parameter server.
+    The Proxy used is ALRobotPosture, in order to make the robot go to the "StandInit" posture
+    at the beginning and at the end of the execution.
+    """
     self.pip = rospy.get_param('pip')
     self.pport = rospy.get_param('pport')
     rospy.loginfo("MasterNode connecting to NaoQi at %s:%d", self.pip, self.pport)
@@ -24,6 +37,12 @@ class MasterNode(NaoqiNode):
     rospy.loginfo("ALRobotPosture successful!")
       
   def start(self):
+    """
+    Actual execution of the node.
+    The directions (right, front, left) are represented as integers (-1, 0, 1).
+    The services LookAt and TakePicture are called successively for each direction.
+    In particular, the TakePicture service must be called when the LookAt service response is True.
+    """
     self.motionProxy.goToPosture("StandInit", 1.0)
     for direction in [-1, 0, 1]:
       # Look at right, front, left
@@ -39,6 +58,11 @@ class MasterNode(NaoqiNode):
     self.motionProxy.goToPosture("StandInit", 1.0)
 
   def _look_at(self, direction):
+    """
+    Calls the LookAt service.
+    The request is the current direction.
+    The response is the success of the service.
+    """
     service = rospy.get_param('look_at_service')
     rospy.wait_for_service(service)
     try:
@@ -49,6 +73,11 @@ class MasterNode(NaoqiNode):
       return False
 
   def _take_picture(self, direction):
+    """
+    Calls the TakePicture service.
+    The request is the current direction.
+    The response is the success of the service.
+    """
     service = rospy.get_param('take_picture_service')
     rospy.wait_for_service(service)
     try:
@@ -60,6 +89,7 @@ class MasterNode(NaoqiNode):
 
 
 def main():
+  """Creates and executes the node."""
   MasterNode().start()
   try:
     rospy.spin()
