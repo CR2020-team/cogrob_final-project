a4b6a2543cdbee973c25b78a540867c471b1dcea
emaff <58251817+emaff@users.noreply.github.com>
2020-11-24 23:23:06 +0100

    Added documentation of the code

--- pepper_pkg/src/camera_node/main.py
+++ pepper_pkg/src/camera_node/main.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
+'''
+This node starts the acquisition of images from Pepper's camera
+and provides the input for the Detection node.
+'''
+
 import rospy
 from naoqi_driver.naoqi_node import NaoqiNode
 from pepper_msgs.msg import ImageWithDirection
@@ -7,12 +12,16 @@ from sensor_msgs.msg import Image
 from std_msgs.msg import Header
 import genpy
 
-
+"""Class used as an abstraction of the Node"""
 class CameraNode(NaoqiNode):
 
   __slots__ = 'videoDeviceProxy', 'videoDevice', '_pub'
 
   def __init__(self):
+    """
+    Constructor. Creates the node, connects it to the NaoQi interface,
+    and makes a subriscription for acquiring the camera.
+    """
     NaoqiNode.__init__(self, 'camera_node')
     self.connectNaoQi()
     cameraID = 0  # CameraID 0 means TopCamera
@@ -21,6 +30,10 @@ class CameraNode(NaoqiNode):
     self.videoDevice = self.videoDeviceProxy.subscribeCamera("pepper_top_camera", cameraID, resolution, colorSpace, 10)
 
   def connectNaoQi(self):
+    """
+    Connects the node to the NaoQi interface. The parameters pip and pport are stored in the parameter server.
+    The Proxy used is ALVideoDevice in order to handle the image from camera
+    """
     self.pip = rospy.get_param('pip')
     self.pport = rospy.get_param('pport')
     rospy.loginfo("CameraNode connecting to NaoQi at %s:%d", self.pip, self.pport)
@@ -30,10 +43,17 @@ class CameraNode(NaoqiNode):
     rospy.loginfo("ALVideoDevice successful!")
   
   def start(self):
+    """
+    Actual execution of the node.
+    It creates a service server for taking pictures and starts to publish on the topic.
+    """
     rospy.Service(rospy.get_param('take_picture_service'), TakePicture, self.handle_take_picture)
     self._pub = rospy.Publisher(rospy.get_param('image_topic'), ImageWithDirection, queue_size=0, latch=True)
 
   def handle_take_picture(self, req):
+    '''
+    Acquires the image from the camera and publishes it on the topic under the ImageWithDirection type.
+    '''
     direction = req.direction
     result = self.videoDeviceProxy.getImageRemote(self.videoDevice)
     if result == None or result[6] == None:
@@ -56,6 +76,7 @@ class CameraNode(NaoqiNode):
     
 
 def main():
+  """Creates and executes the node."""
   CameraNode().start()
   try:
     rospy.spin()
