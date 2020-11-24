3087a1679c96fca651dbfc877e35512401430519
giorge1 <g.ammendola4@studenti.unisa.it>
2020-11-24 23:17:42 +0100

    added docs to speaker node

--- pepper_pkg/src/speaker_node/main.py
+++ pepper_pkg/src/speaker_node/main.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python
+"""
+This node is the one that makes pepper speak. It receives the name of the objects detected by the
+detectore and the direction.
+"""
+
 from collections import Counter
 import rospy
 from naoqi_driver.naoqi_node import NaoqiNode
 from pepper_msgs.msg import DetectionArrayWithDirection
 
-
+"""Class used as an abstraction of the Node"""
 class SpeakerNode(NaoqiNode):
 
   __slots__ = 'animatedSpeechProxy', '_text_components', '_direction_to_text', '_counter'
 
   def __init__(self):
+    """
+    Constructor. Creates the node and connects it to the NaoQi interface.
+    It also initializes internal variables used to create and coordinate what pepper has to say.
+    """
+
     NaoqiNode.__init__(self, 'speaker_node')
     self.connectNaoQi()
     self._direction_to_text = {
@@ -21,6 +31,12 @@ class SpeakerNode(NaoqiNode):
     self._counter = 0
 
   def connectNaoQi(self):
+    """
+    Connects the node to the NaoQi interface. The parameters pip and pport are stored in the parameter server.
+    The Proxy used is ALTextToSpeech, in order to make the robot speech when the sentence is ready. In order to
+    make pepper move while speaking, we are using the animatedSpeech proxy, which will make pepper move according
+    to what it is saying.
+    """
     self.pip = rospy.get_param('pip')
     self.pport = rospy.get_param('pport')    
     rospy.loginfo("SpeakerNode connecting to NaoQi at %s:%d", self.pip, self.pport)
@@ -32,9 +48,17 @@ class SpeakerNode(NaoqiNode):
     rospy.loginfo("ALTextToSpeech successful!")
   
   def start(self):
+    """
+    The only operation performed when the node starts is the subscription to the topic and the linking of the callback.
+    """
     rospy.Subscriber(rospy.get_param('object_list_topic'), DetectionArrayWithDirection, self.rcv_detections_cb)
 
   def rcv_detections_cb(self, msg):
+    """
+    Actual execution of the node. Upon receiving a new message on the topic, it stores a part of the sentence to say. 
+    The TTS will be performed when the three direction are into the sentence to say. In addition, if no object is detected on 
+    one side, pepper will say that it sees nothing.
+    """
     direction = msg.direction
     self._counter += 1
     d = Counter(detection.clabel for detection in msg.detections)
@@ -52,10 +76,14 @@ class SpeakerNode(NaoqiNode):
       self._counter = 0
 
   def _item_to_text(self, key, value):
+    """
+    Utility function that return the objects with the corresponding article or number.
+    """
     return "a {}".format(key) if value == 1 else "{} {}s".format(value, key)
 
 
 def main():
+  """Creates and executes the node."""
   SpeakerNode().start()
   try:
     rospy.spin()
